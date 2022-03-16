ffa352ae7f50e37f3dd71adab983deb46f055e7e
mehmetasilkilic <87938113+mehmetasilkilic@users.noreply.github.com>
2022-03-16 10:05:51 +0300

    A

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from "./components/Header"
+import TodoItem from "./components/TodoItem"
+import AddTodo from "./components/AddTodo"
 
 export default function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+    if (text.length > 2) {
+      setTodos(prevTodos => {
+        return [
+          ...prevTodos,
+          { text: text, key: new Date().getTime() }
+        ]
+      })
+    } else {
+      Alert.alert("OOPS!", "Todos must be over 2 chars long", [
+        { text: "Understood", onPress: () => console.log("Alert closed") }
+      ])
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback >
   );
 }
 
@@ -14,7 +55,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+  item: {
+    marginTop: 24,
+    backgroundColor: 'pink',
+    padding: 30,
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24
   },
 });
